f91918258d996536e2c24e47b2c1039a59820b2f
cemilker <c.karaduman@palmet.com>
2022-10-23 15:22:45 +0300

    Dropdown List tamamlandı.

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
     'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
@@ -47,12 +47,12 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => env('DB2_HOST', '127.0.0.1'),
+            'port' => env('DB2_PORT', '3306'),
+            'database' => env('DB2_DATABASE', 'forge'),
+            'username' => env('DB2_USERNAME', 'forge'),
+            'password' => env('DB2_PASSWORD', ''),
+            'unix_socket' => env('DB2_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Palmet Digital') <!-- şablona bilgi göndermek için -->
 <body>
   <head>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
 <h4>Merhaba,</h4>
 <h5>Palmet "Yardım Masası"" uygulamasına hoş geldiniz!</h5>
@@ -14,11 +16,39 @@
       margin-left:500px;
     }
 </style> -->
-
+<style>
+    select {
+        /* width: 150px;
+        margin: 10px; */
+    }
+    select:focus {
+        /* min-width: 150px; */
+        /* width: auto; */
+    }
+    div.ck1{
+      width:300px;
+      margin-left: 20px;
+    }
+</style>
 
 <h2>Yardım Talep Formu</h2>
 <form method="post">
   @CSRF
+
+    <div class="ck1">
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Talep Türü</option>
+        <option value="swhlp">Yazılım Destek</option>
+        <option value="hwhlp">Donanım Destek</option>
+        <option value="swlcn">Yazılım Lisans</option>
+        <option value="newsw">Yeni Yazılım</option>
+        <option value="newhw">Yeni Donanım</option>
+        <option value="bilgi">Bilgi Talebi</option>
+      </select>
+    </div>
+
+  <br><br><br>
+
   Name:
   <input type="text" name="name">
   <br>
@@ -43,4 +73,5 @@
   <input class="f1" type="submit" name="submit"
     value="Gönder">
 </form>
+
 </body>
